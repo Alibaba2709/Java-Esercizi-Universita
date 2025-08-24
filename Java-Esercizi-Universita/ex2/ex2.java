6bdb56e2af78bf0fef49dd7b79f1df590b9d7a65
Alibaba <ariacondizionata05@gmail.com>
2025-08-24 17:30:58 +0200

    ex2.java

--- Java-Esercizi-Universita/ex2/ex2.java
+++ Java-Esercizi-Universita/ex2/ex2.java
@@ -1,3 +1,14 @@
+/*Scrivere un programma Java che:
+1. 2. Chieda in input all’utente il numero N di parole che si vogliono inserire
+Acquisisca da tastiera N parole in sequenza, chiedendole all’utente una
+alla volta
+3. Per ogni parola acquisita, stampi a video la stringa:
+1.“Parola corta” se la lunghezza della parola è minore di 3;
+2.“Parola di media lunghezza” se la parola ha lunghezza tra 3 e 8;
+3.“Parola lunga” se la lunghezza della parola è maggiore di 8;
+Realizzare la stampa della stringa corretta attraverso un metodo che prende
+in input la lunghezza di una parola acquisita*/
+
 package ex2;
 
 import java.util.Scanner;
